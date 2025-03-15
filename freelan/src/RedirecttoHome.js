d3c21522833e3b93d7d3784469eaa99e9f83c708
Vineet Raj <vineetcres@gmail.com>
2025-03-15 23:32:08 +0530

    Replace freelan with freelan-back directory

--- freelan/src/RedirecttoHome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const RedirecttoHome = () => {
-  return <Navigate to="/" replace />;
-};
-
-export default RedirecttoHome;
